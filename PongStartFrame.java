4edbc5f5b9afe9867994b2fc6423bb5bffd2e97f
AAUSTwiu <103382673+AAUSTwiu@users.noreply.github.com>
2022-11-27 10:53:12 -0600

    Add files via upload

--- PongStartFrame.java
+++ PongStartFrame.java
@@ -1,18 +1,18 @@
-package pong;
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+package pongPackage;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.JPanel;
+
 public class PongStartFrame extends JFrame implements ActionListener{
 	JLabel welcome, credits;
-	JButton play, settings, store, howToPlay;
-PongStartPanel panel1;
+	JButton splay, mplay, settings, shop, howToPlay;
+	PongStartPanel panel1;
 	PongStartFrame(){
 	  	panel1 = new PongStartPanel();
 	  	this.add(panel1);
@@ -23,38 +23,43 @@ PongStartPanel panel1;
 	  	this.pack();
 	  	this.setLocationRelativeTo(null);
 	  	this.setVisible(true);
-	  Color c = new Color(122,235,191);
+	  
 			welcome = new JLabel("Welcome to Pong!");
 		    panel1.add(welcome);
-		    welcome.setFont(new Font("Times New Roman", Font.BOLD, 75));
-		    welcome.setForeground(c);
-		    welcome.setBounds(200,25,600,80);
+		    welcome.setFont(new Font("Times New Roman", Font.BOLD, 57));
+		    welcome.setBounds(250,25,500,75);
 		    welcome.setVisible(true);
 		    
-		    credits= new JLabel("Created by Alex, Lily, & Brodie");
+		    credits= new JLabel("Created by Alex, Brodie, & Lily");
 		    panel1.add(credits);
-		    credits.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		    credits.setBounds(325,100,400,40);
+		    credits.setFont(new Font("Times New Roman", Font.BOLD, 24));
+		    credits.setBounds(315,100,400,40);
 		    credits.setVisible(true);
 		    	
 		    
-		    play = new JButton("PLAY");
-		    panel1.add(play);
-		    play.addActionListener(this);
-		    play.setBounds(380,380,150,65);
-		    play.setVisible(true);
+		    splay = new JButton("Single-Player");
+		    panel1.add(splay);
+		    splay.addActionListener(this);
+		    splay.setBounds(400,200,150,45);
+		    splay.setVisible(true);
+		    
+		    mplay = new JButton("Face-Off");
+		    panel1.add(mplay);
+		    mplay.addActionListener(this);
+		    mplay.setBounds(425,275,100,45);
+		    mplay.setVisible(true);
 		    
 		    settings = new JButton("Settings");
 		    panel1.add(settings);
 		    settings.addActionListener(this);
-		    settings.setBounds(30,25,100,30);
+		    settings.setBounds(295,450,100,30);
 		    settings.setVisible(true);
 		    
-		    store = new JButton("Store");
-		    panel1.add(store);
-		    store.addActionListener(this);
-		    store.setBounds(130,450,100,30);
-		    store.setVisible(true);
+		    shop = new JButton("Shop");
+		    panel1.add(shop);
+		    shop.addActionListener(this);
+		    shop.setBounds(165,450,100,30);
+		    shop.setVisible(true);
 		    
 		    howToPlay = new JButton("How To Play");
 		    panel1.add(howToPlay);
@@ -65,18 +70,17 @@ PongStartPanel panel1;
 	    
 	}
 	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equals("PLAY")) {
-			PongGameFrame frame = new PongGameFrame();
-
+		if(e.getActionCommand().equals("Single-Player")) {
+			
 		}
-		if(e.getActionCommand().equals("Store")) {
-			PongStoreFrame frame = new PongStoreFrame();
-
+		if(e.getActionCommand().equals("Face-Off")) {
+			PongGameFrame foFrame = new PongGameFrame();
 		}
-		
 		if(e.getActionCommand().equals("Settings")) {
-			PongSettingsFrame frame = new PongSettingsFrame();
-
+			PongSettingsFrame sFrame = new PongSettingsFrame();
+		}
+		if(e.getActionCommand().equals("Shop")) {
+			PongStoreFrame stFrame = new PongStoreFrame();
 		}
 	
 	}
